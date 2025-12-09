4803b4a7d0acd51225dea5ac7e7a33b04bc4506b
Mohamed22221 <hamdymohame7@gmail.com>
2025-12-09 14:14:18 +0200

    Initial commit of project files

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
